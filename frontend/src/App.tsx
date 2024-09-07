1cacfd35b7adbc903859aa8ee363ce0532d0b406
Tzikas <niko.tzikas@gmail.com>
2024-09-07 14:59:21 -0400

    mistake

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,23 @@
-import {useState, useEffect, useContext} from 'react';
+import { useState, useEffect, useContext } from 'react';
 import './App.css';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import Profile from './components/Profile';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
-import NoMatch from './components/NoMatch';
-import { GoogleLogin } from '@react-oauth/google';
-import actions from "./api/index";
-import TheContext from './TheContext';
-import { IUser } from './Interfaces';
-
+import NoMatch from './compon
 function App() {
 
-  let [user, setUser] = useState<IUser>({name:'', email:'', picture: ''})
+  let [user, setUser] = useState<IUser>({ name: '', email: '', picture: '' })
 
   const getTheUser = async () => {
     let res = await actions.getUser().catch(console.error)
-    if(res){
+    if (res) {
       setUser(res.data)
-    }    
+    }
   }
   const logOut = () => {
     localStorage.removeItem('token')
-    setUser({name:'', email:'', picture: ''})
+    setUser({ name: '', email: '', picture: '' })
   }
 
   useEffect(() => {
@@ -31,10 +26,10 @@ function App() {
 
 
 
-  return (  
-    <TheContext.Provider value={{getTheUser, user}}>
+  return (
+    <TheContext.Provider value={{ getTheUser, user }}>
       <Routes>
-        <Route path="/" element={<Layout user={user} setUser={setUser} logOut={logOut}/>}>
+        <Route path="/" element={<Layout user={user} setUser={setUser} logOut={logOut} />}>
           <Route index element={<Home />} />
           <Route path="profile" element={<Profile />} />
           <Route path="dashboard" element={<Dashboard />} />
@@ -45,66 +40,66 @@ function App() {
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
-      </TheContext.Provider>
+    </TheContext.Provider>
 
   );
 }
 
 
 
-function Layout(props:any) {  
+function Layout(props: any) {
   let { getTheUser } = useContext(TheContext)
 
   return (
     <>
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-          <h4>Welcome {props.user.name}</h4>
-          <hr />
+      <h4>Welcome {props.user.name}</h4>
+      <hr />
       <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
-          </li>        
+          </li>
           {props.user.name ?
-          <>
+            <>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link onClick={props.logOut} to="">LogOut</Link>
+              </li>
+            </>
+            :
             <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li> 
-            <Link onClick={props.logOut} to="">LogOut</Link>
-            </li>   
-            </>  
-           :
-           <li>
-            <GoogleLogin
-              onSuccess={async credentialResponse => {
-                await actions.authenticate(credentialResponse)
-                await getTheUser()
-              }}
-              onError={() => {
-                console.log('Login Failed');
-              }}
-              auto_select
-            />
+              <GoogleLogin
+                onSuccess={async credentialResponse => {
+                  await actions.authenticate(credentialResponse)
+                  await getTheUser()
+                }}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+                auto_select
+              />
             </li>
           }
 
-       
+
         </ul>
-        
+
       </nav>
-      
+
       <hr />
 
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
       <Outlet />
-      </>
+    </>
 
   );
 }
